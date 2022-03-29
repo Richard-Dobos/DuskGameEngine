057514083c52ae5b57daff3284527ef38af4807c
Richard <89549164+mastarious667@users.noreply.github.com>
2022-03-29 22:38:32 +0200

    And fixed bug with log.c where client logger wasn't initialized

--- DuskGameEngine/src/Dusk/Log.h
+++ DuskGameEngine/src/Dusk/Log.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include"Core.h"
+
 #include"spdlog/spdlog.h"
 #include"spdlog/sinks/stdout_color_sinks.h"
+#include"spdlog/fmt/ostr.h"
 
 #include<memory>
 
@@ -27,11 +29,9 @@ namespace Dusk
 #define DK_CORE_TRACE(...) ::Dusk::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define DK_CORE_WARN(...) ::Dusk::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define DK_CORE_ERROR(...) ::Dusk::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define DK_CORE_FATAL(...) ::Dusk::Log::GetCoreLogger()->fatal(__VA_ARGS__)
 
 //App log macro
 #define DK_CORE_INFO(...) ::Dusk::Log::GetClientLogger()->info(__VA_ARGS__)
 #define DK_CORE_TRACE(...) ::Dusk::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define DK_CORE_WARN(...) ::Dusk::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define DK_CORE_ERROR(...) ::Dusk::Log::GetClientLogger()->error(__VA_ARGS__)
-#define DK_CORE_FATAL(...) ::Dusk::Log::GetCoreLogger()->fatal(__VA_ARGS__)
